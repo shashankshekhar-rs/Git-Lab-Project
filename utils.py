fe2d809f2351b1799fe5bd08e6825e9bd52fc7b6
shashankshekhar-rs <srcschoolpatna@gmail.com>
2026-09-15 18:48:03 +0530

    Implement student list with duplicate removal
    Add a list of students and remove duplicates.
    radhe shyam

---
+++ utils.py
@@ -0,0 +1,12 @@
+students=[
+    "radha",
+    "radha",
+    "shyama",
+    "satya",
+    "ramya"]
+print(students[1])
+for i in students:
+
+  if i==students[1]:
+      students.remove(i)
+print(students)
